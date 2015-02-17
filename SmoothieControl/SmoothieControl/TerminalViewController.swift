fc35e28b69ecad20228f669e15cfe2275c7c6061
quillford <adeebshihadeh@gmail.com>
2015-02-16 23:20:13 -0800

    Basic UI added

---
+++ SmoothieControl/SmoothieControl/TerminalViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TerminalViewController.swift
+//  SmoothieControl
+//
+//  Created by quillford on 2/16/15.
+//  Copyright (c) 2015 quillford. All rights reserved.
+//
+
+import UIKit
+
+class TerminalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
